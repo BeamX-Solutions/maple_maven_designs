b220da0af7b69b521cdbce3050b36b97b1dd2fa2
chimaobi-ibeh <ibehchimaobi98@gmail.com>
2025-06-04 22:28:13 +0100

    Fixed the margin issue on the Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -161,7 +161,7 @@ const HomePage: React.FC = () => {
       {/* Why Choose Us Section */}
       <section
         ref={sectionRef}
-        className="py-16 mt-8 bg-gray-50"
+        className="py-16 mt-0 bg-gray-50"
       >
         <div className="container mx-auto px-4 md:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 items-start">
